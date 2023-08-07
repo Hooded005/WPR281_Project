ecd20acc32fb45b4f6503676dc72f7876aa21e38
Sean Botha <seanbotha2003@gmail.com>
2023-08-07 16:42:07 +0200

    Test

---
+++ project.js
@@ -0,0 +1,12 @@
+const firstname = document.getElementById("firstname");
+const email = document.getElementById("email");
+const lastname = document.getElementById("lastname");
+const phone = document.getElementById("phone");
+const userid = document.getElementById("userID");
+const refcode = document.getElementById("refcode");
+const form = document.getElementById("form");
+
+form.addEventListener('submit', function(e)
+{
+    e.preventDefault();
+})
